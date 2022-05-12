5eb6342972e47fb10eb842b6a42b01bac01bbf5e
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-13 00:46:12 +0430

    Implement random number picking

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int randInRange(int min, int max);
 
 int main(int argc, char *argv[]) {
-  printf("Hello world!\n");
+  srand(time(0));
+
+  const int MIN = 1;
+  const int MAX = 10;
+
+  int random = randInRange(MIN, MAX);
+  printf("HINT: %d is chosen.\n", random);
+
   return 0;
 }
+
+int randInRange(int min, int max) { return rand() % (max - min + 1) + min; }
